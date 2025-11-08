18b3eb1fb4b3dcd4c11ae026bfff6c1d0dd400bd
Aj149 <adryjosu149@gmail.com>
2025-11-08 16:27:22 -0500

    Añadir .gitignore para ignorar .env

--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -158,5 +158,3 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazo
 
 MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-print(f"INFO: Usando S3 bucket '{AWS_STORAGE_BUCKET_NAME}' para media.")
